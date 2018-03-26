cc7fba89ad916580fb08cebee726635534f57c46
dinhphu98 <dinhphu98@dinhphu98s-MacBook-Pro.local>
2018-03-26 09:06:04 +0700

    Initial Commit

---
+++ NoDaTa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoDaTa
+//
+//  Created by dinhphu98 on 3/26/18.
+//  Copyright © 2018 dinhphu98. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
